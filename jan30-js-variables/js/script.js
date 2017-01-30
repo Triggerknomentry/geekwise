0451cf9b22aff75fcb99f143b6d6d19c3625a19c
student-08 <student-08>
2017-01-30 15:49:33 -0800

    jan30-js-variables, Pokemon Gold is best

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,28 @@
+//alert('mad cow');
+
+/*
+comment
+out
+multiple
+lines
+*/
+
+var myNumberVar = 8; //Number Object
+var myFloatVar = 5.2; //Float Object
+var myStringVar = 'Rigoberto Padilla'; //String Object
+var myBooleanVar = true; //Boolean Object
+var myArrayVar = ['one', 'stingray', 'banana', 8, true] //Array Object
+var myObjectVar = {
+    make: 'Toyota',
+    model: 'Tacoma',
+    color: 'red',
+    year: 2001
+}; //Object Object
+
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar)
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myArrayVar);
+console.log(myObjectVar.model );
