59623b86ed895b078de39cf6d9eccbe77d824a1a
student-08 <student-08>
2017-02-17 15:11:58 -0800

    feb17-js-loops, Windows 10 sucks

---
+++ feb17-js-loops/js/script.js
@@ -0,0 +1,22 @@
+var btn = document.getElementById('btn');
+var movieArr = [];
+
+btn.addEventListener('click', getMovie;
+
+function getMovie(){
+  movieArr.push( prompt('whats your favorite movie') );
+  if(movieArr.length >= 3){
+    console.log(movieArr);
+  }
+  if(movieArr.length >= 5){
+    // for(var i = 0; i < movieArr.length; ){
+      // console.log(movieArr[i]);
+      movieArr.forEach(function(el, idx){
+        console.log(idx);
+      });
+  }
+
+
+}
+
+Array []
